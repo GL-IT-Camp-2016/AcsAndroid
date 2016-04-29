dc64d8fe45b1263954260bb7b1880b5fc5bb053c
Patrik Obertik <patrik.obertik@rec-global.com>
2016-04-29 17:01:11 +0200

    geofence refactor to service

---
+++ app/src/main/java/com/gl/accesscontrolsystem/GeofenceErrorMessages.java
@@ -0,0 +1,36 @@
+package com.gl.accesscontrolsystem;
+
+/**
+ * Created by Patrik Obertik on 29. 4. 2016.
+ */
+import android.content.Context;
+import android.content.res.Resources;
+
+import com.google.android.gms.location.GeofenceStatusCodes;
+
+/**
+ * Geofence error codes mapped to error messages.
+ */
+public class GeofenceErrorMessages {
+    /**
+     * Prevents instantiation.
+     */
+    private GeofenceErrorMessages() {}
+
+    /**
+     * Returns the error string for a geofencing error code.
+     */
+    public static String getErrorString(Context context, int errorCode) {
+        Resources mResources = context.getResources();
+        switch (errorCode) {
+            case GeofenceStatusCodes.GEOFENCE_NOT_AVAILABLE:
+                return "not_available";
+            case GeofenceStatusCodes.GEOFENCE_TOO_MANY_GEOFENCES:
+                return "geofence_too_many_geofences";
+            case GeofenceStatusCodes.GEOFENCE_TOO_MANY_PENDING_INTENTS:
+                return "geofence_too_many_pending_intents";
+            default:
+                return "unknown_geofence_error";
+        }
+    }
+}
