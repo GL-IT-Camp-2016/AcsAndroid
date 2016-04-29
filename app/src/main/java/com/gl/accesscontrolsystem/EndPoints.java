360e58f4a9a164977f505739f3587950d24907a5
Martin Noga <martin.nogi@gmail.com>
2016-04-29 10:22:26 +0200

    added endpoints constants

---
+++ app/src/main/java/com/gl/accesscontrolsystem/EndPoints.java
@@ -0,0 +1,13 @@
+package com.gl.accesscontrolsystem;
+
+/**
+ * Created by Martin Noga on 29. 4. 2016.
+ */
+public class EndPoints {
+
+    public static final String ATTENDANCE_ARRIVAL = "/attendance/store/arrival/";
+    public static final String ATTENDANCE_DEPARTURE = "/attendance/store/departure/";
+    public static final String ATTENDANCE_LIST = "/attendance/list/";
+    public static final String PERSON_LIST = "/person/list/";
+
+}
